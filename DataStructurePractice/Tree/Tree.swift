37aed47033ee92c47fbf5672a0bd10a746762191
Dipesh <dipesh.pokhrel@mphasis.com>
2020-05-01 10:58:33 +0530

    Created test node
    Created test node

---
+++ DataStructurePractice/Tree/Tree.swift
@@ -0,0 +1,15 @@
+//
+//  Tree.swift
+//  DataStructurePractice
+//
+//  Created by Dipesh Pokhrel on 5/1/20.
+//  Copyright © 2020 Dipesh Pokhrel. All rights reserved.
+//
+
+class TreeNode {
+    var data : Int?
+    var leftNode : TreeNode?
+    var rightNode : TreeNode?
+}
+
+
